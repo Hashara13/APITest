bcbf7d1769ae3ed21434087371b951325e27834e
Hashara13 <hnethmin13@gmail.com>
2025-02-01 10:31:47 +0530

    Add project files.

---
+++ APITest/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using APITest.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APITest.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        // pass constructe with the ApplicationDbcontext parameter & instance to base class as well
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+    }
+        //Property set for Dbset type for DB collection
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
